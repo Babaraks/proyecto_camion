9806a97e58787c021b12e135d36e8dc85a177d97
Guzman Dynastie <guzmanjrpro@gmail.com>
2024-10-16 12:40:50 -0600

    feat: se agrega clase Stop a Route

--- lib/models/route_model.dart
+++ lib/models/route_model.dart
@@ -6,6 +6,7 @@ class Route {
   final String vehicleType;
   final String passingFrecuency;
   final String vehicleColor;
+  final List<Stop> stops;
 
   Route({
     required this.idRoute,
@@ -14,10 +15,14 @@ class Route {
     required this.lastDepartureTime,
     required this.vehicleType,
     required this.passingFrecuency,
-    required this.vehicleColor
+    required this.vehicleColor,
+    required this.stops,
   });
 
   factory Route.fromJson(Map<String, dynamic> json) {
+    var stopsFromJson = json['stops'] as List;
+    List<Stop> stopList = stopsFromJson.map((i) => Stop.fromJson(i)).toList();
+
     return Route(
       idRoute: json['id_route'],
       nameRoute: json['name_route'],
@@ -26,7 +31,53 @@ class Route {
       vehicleType: json['type_vehicle'],
       passingFrecuency: json['passing_frecuency'],
       vehicleColor: json['type_color'],
+      stops: stopList,
+    );
+  }
+}
+
+class Stop {
+  final int idStop;
+  final String nameStop;
+  final String locationCoordinates;
+  final String district;
+  final int? idEvent;
+  final String? eventName;
+  final String? eventDate;
+  final String? startTimeEvent;
+  final String? endTimeEvent;
+  final String? eventStatus;
+  final int? followUp;
+
+  Stop({
+    required this.idStop,
+    required this.nameStop,
+    required this.locationCoordinates,
+    required this.district,
+    this.idEvent,
+    this.eventName,
+    this.eventDate,
+    this.startTimeEvent,
+    this.endTimeEvent,
+    this.eventStatus,
+    this.followUp,
+  });
+
+  factory Stop.fromJson(Map<String, dynamic> json) {
+    return Stop(
+      idStop: json['id_stop'],
+      nameStop: json['name_stop'],
+      locationCoordinates: json['location_coordinates0'],
+      district: json['district'],
+      idEvent: json['id_event'],
+      eventName: json['event_name'],
+      eventDate: json['event_date'],
+      startTimeEvent: json['start_time_event'],
+      endTimeEvent: json['end_time_event'],
+      eventStatus: json['event_status'],
+      followUp: json['follow_up']
     );
   }
 
 }
+
